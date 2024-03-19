ff032589912511a6174e3e95475d96e0ab568aef
Artur <a.kocharyan1997@gmail.com>
2024-03-20 00:51:46 +0400

    menu mb

--- src/pages/Navigation/Navigation.jsx
+++ src/pages/Navigation/Navigation.jsx
@@ -1,63 +1,94 @@
 import React, { useEffect, useState } from "react";
-import logo from '../../assets/img/image2.svg'
-import search from '../../assets/img/Search2.svg'
-import { RiCloseCircleFill } from "react-icons/ri"
-import style from './style.module.css'
+import logo from "../../assets/img/image2.svg";
+import search from "../../assets/img/Search2.svg";
+import { RiCloseCircleFill } from "react-icons/ri";
+import style from "./style.module.css";
 import DropDownLang from "./DropDownLang/DropDownLang";
 import CitesDropDown from "./CitesDropDown/CitesDropDown";
 import { useSelector } from "react-redux";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Navigation() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputIsEmpty, setInputIsEmpty] = useState(false);
+  const citiesList = useSelector((state) => state.cities);
+  const [mobailMenu, setMobailMenu] = useState(false);
 
-    const [inputValue, setInputValue] = useState('')
-    const [inputIsEmpty, setInputIsEmpty] = useState(false)
-    const citiesList = useSelector((state) => state.cities)
+  useEffect(() => {
+    if (inputValue.length !== 0) {
+      setInputIsEmpty(true);
+    } else {
+      setInputIsEmpty(false);
+    }
+  }, [inputValue]);
 
-    useEffect(() => {
-        if(inputValue.length !== 0) {
-            setInputIsEmpty(true)
-        }else{
-            setInputIsEmpty(false)
-        }
-    },[inputValue])
+  console.log(mobailMenu);
 
-    return (
-        <div className={style.mainContainer} >
-            <div className={style.iconContainer} >
-                <div className={style.icon} >
-                    <img src={logo} alt='Logo' />
-                </div>
-                <div className={style.countriesContainer} >
-                    <CitesDropDown list={citiesList} />
-                </div>
-                
-            </div>
-            <div className={style.searchContainer} >
-                <span> <img src={search} alt='search'/> </span>
-                <input 
-                placeholder="MOVE"
-                value={inputValue}
-                onChange={(e)=>{
-                    e.preventDefault()
-                    setInputValue(e.target.value)
-                }} />
-                <span
-                onClick={(e)=> {
-                    e.preventDefault()
-                    setInputValue('')}}
-                className={!inputIsEmpty ? style.closeSpanHied :style.closeSpan} 
-                > <RiCloseCircleFill /> </span>
-            </div>
-            <div className={style.accountContainer} >
-                <div className={style.lengContainer}>
-                    <DropDownLang />
-                </div>
-                <div className={style.singContainer}>
-                    <button>Sing In</button>
-                </div>
-            </div>
+  return (
+    <div className={style.mainContainer}>
+      <div className={style.iconContainer}>
+        <div className={style.icon}>
+          <img src={logo} alt="Logo" />
         </div>
-    )
+        <div className={style.countriesContainer}>
+          <CitesDropDown list={citiesList} />
+        </div>
+      </div>
+      <div className={style.searchContainer}>
+        <span>
+          {" "}
+          <img src={search} alt="search" />{" "}
+        </span>
+        <input
+          placeholder="MOVE"
+          value={inputValue}
+          onChange={(e) => {
+            e.preventDefault();
+            setInputValue(e.target.value);
+          }}
+        />
+        <span
+          onClick={(e) => {
+            e.preventDefault();
+            setInputValue("");
+          }}
+          className={!inputIsEmpty ? style.closeSpanHied : style.closeSpan}
+        >
+          {" "}
+          <RiCloseCircleFill />{" "}
+        </span>
+      </div>
+      <div className={style.accountContainer}>
+        <div className={style.lengContainer}>
+          <DropDownLang />
+        </div>
+        <div className={style.singContainer}>
+          <button>Sing In</button>
+        </div>
+      </div>
+      <div
+        onClick={() => setMobailMenu(!mobailMenu)}
+        className={style.burgerMenu}
+      >
+        <span>
+          <RxHamburgerMenu />
+        </span>
+      </div>
+      {mobailMenu === true && (
+        <div className={style.menuBody}>
+          <div>
+            <CitesDropDown list={citiesList} />{" "}
+          </div>
+          <div>
+            <DropDownLang />
+          </div>
+          <div className={style.singContainer}>
+            <button>Sing In</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Navigation
+export default Navigation;
